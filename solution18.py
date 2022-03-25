66d927fa7deb9485730c1e8e5234cad57df9f1f8
21bca07 <96609167+21bca07@users.noreply.github.com>
2022-03-25 10:42:36 +0530

    Add files via upload

---
+++ solution18.py
@@ -0,0 +1,12 @@
+#WAS to enter 5 string in a list and check and print string those lenth has even no of count.
+def strlen(s):
+    count=0
+    for i in s:
+        count+=1
+    return(count)
+for i in range(5):
+    str=input("string:")
+    ans=strlen(str)
+    if(ans%2==0):
+        print(str)
+
